e123d48c52d0d81a2743deed5e2bcd7b7c501cae
Sébastien Lampazona <sebastien@edusign.fr>
2024-07-16 12:13:42 +0200

    Première base de l'affichage de la vue étudiant

---
+++ amd/src/pages/student/view-list.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-async-promise-executor */
+/* eslint-disable promise/no-nesting */
+import Ajax from 'core/ajax';
+import * as Str from 'core/str';
+
+
+let student = null;
+const removeSession = function(sessionId, removeOnEdusign = true) {
+    return Ajax.call([{
+        methodname: 'mod_edusign_remove_session',
+        args: {
+            sessionId,
+            withEdusignDelete: removeOnEdusign,
+        }
+    }])[0];
+};
+
+export const init = async(_student) => {
+    student = _student;
+    // eslint-disable-next-line no-console
+    console.log('init student view list', student);
+};
---
+++ session.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * Prints edusign info for particular user
+ *
+ * Moodle will automatically generate links to view the activity using the /view.php page and passing in an id value.
+ * The id passed is the course module ID, which can be used to fetch all remaining data for the activity instance.
+ *
+ * @package    mod_edusign
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(dirname(__FILE__) . '/../../config.php');
+
+$id           = required_param('id', PARAM_INT);
+$sessionId    = required_param('sessionId', PARAM_INT);
+
+$cm           = get_coursemodule_from_id('edusign', $id, 0, false, MUST_EXIST);
+$course       = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
+$edusign      = $DB->get_record('edusign', array('id' => $cm->instance), '*', MUST_EXIST);
+
+require_login($course, true, $cm);
+
+$context = context_module::instance($cm->id);
+
+$PAGE->set_heading($course->fullname);
+$PAGE->set_cacheable(true);
+
+
+$canManageAttendance = array(
+    'mod/edusign:changeattendances',
+    'mod/edusign:manageattendances'
+);
+
+$canTakeAttendance = array(
+    'mod/edusign:takeattendances',
+);
+
+// Si on a une session on redirige vers la page de la session
+if ($sessionId) {
+    if (has_any_capability($canManageAttendance, $context)) {
+        redirect(new moodle_url('/mod/edusign/take.php', ['sessionId' => $sessionId]));
+    } else if (has_any_capability($canTakeAttendance, $context)) {
+        redirect(new moodle_url('/mod/edusign/take.php', ['sessionId' => $sessionId]));
+    }
+} else if (has_any_capability($canManageAttendance, $context)) {
+    redirect(new moodle_url('/mod/edusign/manage.php', ['id' => $cm->id]));
+}
+
+$session = $DB->get_record('edusign_sessions', ['id' => $sessionId]);
+
+$PAGE->set_cm($cm);
+$PAGE->set_context($context);
+$PAGE->set_title($course->shortname . ": " . $att->name);
+$PAGE->set_heading($course->fullname);
+$PAGE->set_cacheable(true);
+
+$PAGE->requires->js_call_amd('mod_edusign/pages/student/take', 'init', [
+    'student' => $USER,
+    'course' => $course,
+    'sessions' => $sessions,
+    'session' => $session,
+]);
+
+$output = $OUTPUT->render_from_template('mod_edusign/student-take', [
+    'title' => $title,
+    'instance' => $cm,
+    'id' => $id,
+    'url' => $url,
+    'context' => $context,
+    'course' => $course,
+    'session' => $session,
+    'PAGE' => $PAGE,
+    'OUTPUT' => $OUTPUT,
+    'CFG' => $CFG,
+    'DB' => $DB,
+    'USER' => $USER,
+    'PAGE' => $PAGE,
+]);
+
+
+echo $OUTPUT->header();
+echo $output;
+echo $OUTPUT->footer();
