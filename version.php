5604f970432c527db546f0265f89f202177eef84
Sébastien Lampazona <sebastien@edusign.fr>
2024-10-09 10:33:05 +0200

    chore(Version): incrementing internal plugin version

--- version.php
+++ version.php
@@ -9,7 +9,7 @@
 defined('MOODLE_INTERNAL') || die();
 $composer = json_decode(file_get_contents(__DIR__ . '/composer.json'));
 
-$plugin->version   = 2024111216;      // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2024111217;      // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2023042408;      // Requires this Moodle version.
 $plugin->component = 'mod_edusign';   // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_BETA;
